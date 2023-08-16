2ee83f048365a26bbe6f6eeb0442dcf03b4913ae
blicuadriDev <blicuadriDev@gmail.com>
2023-08-16 08:41:10 -0300

    GKCommercec project started

---
+++ src/main/java/com/godknows/gkcommerce/GkcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.godknows.gkcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GkcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GkcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/godknows/gkcommerce/GkcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godknows.gkcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GkcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
