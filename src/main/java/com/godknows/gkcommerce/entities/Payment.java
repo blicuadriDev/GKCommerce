af0cb2d24f3da85643bd54b1890d046d1df97fa1
blicuadriDev <blicuadriDev@gmail.com>
2023-08-16 09:37:44 -0300

    Entitie Payment created and set its relation to Order

--- src/main/java/com/godknows/gkcommerce/entities/Payment.java
+++ src/main/java/com/godknows/gkcommerce/entities/Payment.java
@@ -2,45 +2,37 @@ package com.godknows.gkcommerce.entities;
 
 import java.time.Instant;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="tb_order")
-public class Order {
+@Table(name = "tb_payment")
+public class Payment {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant moment;
-	private OrderStatus status;
 	
-	@ManyToOne
-	@JoinColumn(name="client_id")
-	private User client;
+	@OneToOne
+	@MapsId
+	private Order order;
 	
-	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
-	private Payment payment;
-	
-	
-	public Order() {
+	public Payment() {
 	}
 
-	public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
+	public Payment(Long id, Instant moment, Order order) {
 		this.id = id;
 		this.moment = moment;
-		this.status = status;
-		this.client = client;
-		this.payment = payment;
+		this.order = order;
 	}
 
 	public Long getId() {
@@ -59,20 +51,13 @@ public class Order {
 		this.moment = moment;
 	}
 
-	public OrderStatus getStatus() {
-		return status;
+	public Order getOrder() {
+		return order;
 	}
 
-	public void setStatus(OrderStatus status) {
-		this.status = status;
+	public void setOrder(Order order) {
+		this.order = order;
 	}
-
-	public User getClient() {
-		return client;
-	}
-
-	public void setClient(User client) {
-		this.client = client;
-	}
+	
 
 }
