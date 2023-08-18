e4064c8cbb4eb6cd1b0677889bb34d3c05b1371a
blicuadriDev <blicuadriDev@gmail.com>
2023-08-18 11:15:32 -0300

    Get Produts paged

--- src/main/java/com/godknows/gkcommerce/controllers/ProductController.java
+++ src/main/java/com/godknows/gkcommerce/controllers/ProductController.java
@@ -1,6 +1,8 @@
 package com.godknows.gkcommerce.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +22,11 @@ public class ProductController {
 	public ProductDTO findById(@PathVariable Long id){
 		ProductDTO dto =  productService.findById(id);
 		return dto;
+	}
+	
+	@GetMapping
+	public Page<ProductDTO> findAll(Pageable pageable){
+		return productService.findAll(pageable);
 	}
 
 }
--- src/main/java/com/godknows/gkcommerce/services/ProductService.java
+++ src/main/java/com/godknows/gkcommerce/services/ProductService.java
@@ -3,6 +3,8 @@ package com.godknows.gkcommerce.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -30,5 +32,11 @@ public class ProductService {
 	 * 		return new ProductDTO(product);
 	 * 	}
 	 */
+	
+	@Transactional(readOnly = true)
+	public Page<ProductDTO> findAll(Pageable pageable) {
+		Page<Product> result = productRepository.findAll(pageable);
+		return result.map(x -> new ProductDTO(x));
+	}
 
 }
