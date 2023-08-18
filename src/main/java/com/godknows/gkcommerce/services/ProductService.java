073289b2a5c92daafc33edae8fbd3f03595a7a23
blicuadriDev <blicuadriDev@gmail.com>
2023-08-18 13:04:05 -0300

    Put Product

--- src/main/java/com/godknows/gkcommerce/services/ProductService.java
+++ src/main/java/com/godknows/gkcommerce/services/ProductService.java
@@ -43,14 +43,27 @@ public class ProductService {
 	@Transactional
 	public ProductDTO insert(ProductDTO dto) {
 		Product entity = new Product();
+		copyToDto(dto, entity);
+		entity = productRepository.save(entity);
+		return new ProductDTO(entity);
+	}
+	
+	
+	@Transactional
+	public ProductDTO update(Long id, ProductDTO dto) {
+		Product entity = productRepository.getReferenceById(id);
+		copyToDto(dto, entity);
+		entity = productRepository.save(entity);
+		return new ProductDTO(entity);
+	}
+	
+	
+	
+	private void copyToDto (ProductDTO dto, Product entity) {
 		entity.setName(dto.getName());
 		entity.setDescription(dto.getDescription());
 		entity.setPrice(dto.getPrice());
 		entity.setImgUrl(dto.getImgUrl());
-		
-		entity = productRepository.save(entity);
-		
-		return new ProductDTO(entity);
 	}
 
 }
