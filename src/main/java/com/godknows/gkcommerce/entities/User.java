c3ddb07db4cee315b3d8e806a3ecb8fddc22e393
blicuadriDev <blicuadriDev@gmail.com>
2023-08-21 08:20:03 -0300

    Fist step to jwt oauth2 spring security: Domain model User Role + seed

--- src/main/java/com/godknows/gkcommerce/entities/User.java
+++ src/main/java/com/godknows/gkcommerce/entities/User.java
@@ -2,14 +2,19 @@ package com.godknows.gkcommerce.entities;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
@@ -30,6 +35,12 @@ public class User {
 	
 	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
+	
+	@ManyToMany
+	@JoinTable(name = "tb_user_role",
+			joinColumns = @JoinColumn(name="user_id"),
+			inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
 	
 	
 	public User() {
@@ -108,6 +119,26 @@ public class User {
 
 	public List<Order> getOrders() {
 		return orders;
+	}
+
+
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+	
+	
+	public void addRole(Role role) {
+		roles.add(role);
+	}
+	
+	public boolean hasRole(String rolename) {
+		for(Role role : roles) {
+			if(role.getAuthority().equals(rolename)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 
