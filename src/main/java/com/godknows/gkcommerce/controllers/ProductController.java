fc6b2520f3b2098000a76c53ff4db6b1f7934b2a
blicuadriDev <blicuadriDev@gmail.com>
2023-08-21 09:25:27 -0300

    Access control by route vs users role

--- src/main/java/com/godknows/gkcommerce/controllers/ProductController.java
+++ src/main/java/com/godknows/gkcommerce/controllers/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -44,6 +45,7 @@ public class ProductController {
 	}
 	
 	
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 	@PostMapping
 	public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto){
 		dto = productService.insert(dto);
@@ -52,6 +54,7 @@ public class ProductController {
 	}
 	
 	
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 	@PutMapping(value="{id}")
 	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO dto){
 		dto =  productService.update(id, dto);
@@ -59,6 +62,7 @@ public class ProductController {
 	}
 	
 	
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 	@DeleteMapping(value="{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id){
 		productService.delete(id);
