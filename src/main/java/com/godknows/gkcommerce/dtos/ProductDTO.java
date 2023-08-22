4a274411e27dba73a5908b86854b7132b6e1bbd5
blicuadriDev <blicuadriDev@gmail.com>
2023-08-22 13:33:16 -0300

    Adjustments to vvalidation

--- src/main/java/com/godknows/gkcommerce/dtos/ProductDTO.java
+++ src/main/java/com/godknows/gkcommerce/dtos/ProductDTO.java
@@ -8,6 +8,7 @@ import com.godknows.gkcommerce.entities.Product;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
@@ -22,6 +23,7 @@ public class ProductDTO {
 	@Size(min = 10, message ="Descrição precisa ter pelo menos 10 caracteres")
 	private String description;
 	
+	@NotNull(message = "Campo obrigatório.")
 	@Positive(message="O preço tem de ser um valor positivo")
 	private Double price;
 	
