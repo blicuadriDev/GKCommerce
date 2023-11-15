63bc840bd86b44afcdfeda04e1c7ca2c334ed781
blicuadriDev <blicuadriDev@gmail.com>
2023-11-15 16:15:48 -0300

    ProductService findAll unit test

--- src/test/java/com/godknows/gkcommerce/services/ProductServiceTest.java
+++ src/test/java/com/godknows/gkcommerce/services/ProductServiceTest.java
@@ -1,5 +1,8 @@
 package com.godknows.gkcommerce.services;
 
+import static org.mockito.ArgumentMatchers.any;
+
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
@@ -9,9 +12,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.godknows.gkcommerce.dtos.ProductDTO;
+import com.godknows.gkcommerce.dtos.ProductMinDTO;
 import com.godknows.gkcommerce.entities.Product;
 import com.godknows.gkcommerce.factories.ProductFactory;
 import com.godknows.gkcommerce.repositories.ProductRepository;
@@ -27,8 +35,11 @@ public class ProductServiceTest {
 	private ProductRepository repository;
 	
 	private Long existingId, unexistingId;
-	private Product product;
 	private String productName;
+	
+	private Product product;
+	private PageImpl<Product> page;
+
 	
 	@BeforeEach
 	private void setUp() {
@@ -36,9 +47,12 @@ public class ProductServiceTest {
 		unexistingId = 2L;
 		productName = "PlyStation 5";
 		product = ProductFactory.createProduct(productName);
+		page = new PageImpl<>(List.of(product));
 		
 		Mockito.when(repository.findById(existingId)).thenReturn(Optional.of(product));
 		Mockito.when(repository.findById(unexistingId)).thenReturn(Optional.empty());
+		
+		Mockito.when(repository.searchByName(any(),(Pageable)any())).thenReturn(page);
 	}
 	
 	
@@ -58,6 +72,17 @@ public class ProductServiceTest {
 		Assertions.assertThrows(ResourceNotFoundException.class, ()->{
 			service.findById(unexistingId);
 		});
+	}
+	
+	@Test
+	public void findAllShouldReturnPagedProductMinDTO() {
+		Pageable pageable = PageRequest.of(0, 12);
+		
+		Page<ProductMinDTO> result = service.findAll(productName, pageable);
+		
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(result.getSize(), 1);
+		Assertions.assertEquals(result.iterator().next().getName(), productName);
 	}
 
 }
